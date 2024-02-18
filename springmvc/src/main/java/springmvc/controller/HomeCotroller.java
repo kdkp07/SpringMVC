dca2cc36c34a3159f3518d9939376a37534f2901
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2024-02-18 16:02:26 +0530

    Added help page

--- springmvc/src/main/java/springmvc/controller/HomeCotroller.java
+++ springmvc/src/main/java/springmvc/controller/HomeCotroller.java
@@ -1,14 +1,26 @@
 package springmvc.controller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeCotroller {
 
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("We are in home controller function");
+		model.addAttribute("name","Kuldeep Patidar");
+		model.addAttribute("Id" , 123124);
+
+		// trying to add a collection of objects
+		ArrayList<String> li = new ArrayList<String>();
+		li.add("C++");li.add("Java");li.add("Python");
+		model.addAttribute("Languages",li);
 		return "index";
 	}
 
@@ -22,5 +34,18 @@ public class HomeCotroller {
 	public String services() {
 		System.out.println("this is services page");
 		return "services";
+	}
+
+	@RequestMapping("/help")
+	public ModelAndView help() {
+		System.out.println("Inside help function");
+		ModelAndView model = new ModelAndView();
+		model.addObject("name","Aakash Bhalla");
+		model.addObject("RollNo",201);
+		model.addObject("isHungry",false);
+		LocalDateTime now = LocalDateTime.now();
+		model.addObject("time",now);
+		model.setViewName("help");
+		return model;
 	}
 }
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ page import="java.util.List" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +9,22 @@
 </head>
 <body>
 	<h1>This is a home page</h1>
-	<h3>Home to many</h3>
+	<%
+	String name = (String)request.getAttribute("name");
+	Integer id = (Integer)request.getAttribute("Id");
+	List<String> li =(List<String>)request.getAttribute("Languages");
+	
+	%>
+	
+	<h1>Name is <%=name%></h1>
+	<h2>Id is <%= id %></h2>
+	
+	<% for(String s : li){
+		%>
+		<h1><%=s %></h1>
+	<%
+	}
+	%>
+	
 </body>
 </html>
