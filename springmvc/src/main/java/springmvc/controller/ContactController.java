53cd65acc25cefc830993e30ac615a6bda88dfc5
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2024-02-24 16:08:08 +0530

    Added User class to handle data simultaneously.

--- springmvc/src/main/java/springmvc/controller/ContactController.java
+++ springmvc/src/main/java/springmvc/controller/ContactController.java
@@ -15,14 +15,29 @@ public class ContactController {
 
 	@RequestMapping(path = "/processform",method = RequestMethod.POST)
 	public String handleForm(@RequestParam("email") String email , @RequestParam("name") String name , @RequestParam("password") String password,Model model) {
+
+		// here we have got 3 tasks to do 
+		// 1. Getting user data from view to controller (contact html form ) 
+		// 2. Fetching this data to view  (@RequestForm)
+		// 3. Viewing this data to other view , here it is "success.jsp" {model.addAttribute("password",password);}
+
+		// what if we can do this using only one thing . Its given below.
+
 		System.out.println(email);
 		System.out.println(name);
 		System.out.println(password);
 
-		model.addAttribute("email",email);
-		model.addAttribute("name",name);
-		model.addAttribute("password",password);
+		//		User user = new User();
+		//		user.setEmail(email);
+		//		user.setName(name);
+		//		user.setPassword(password);
 
+
+		//		model.addAttribute("email",email);
+		//		model.addAttribute("name",name);
+		//		model.addAttribute("password",password);
+
+		//		model.addAttribute("user" ,user);
 
 		return "success";
 	}
--- springmvc/src/main/webapp/WEB-INF/views/success.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/success.jsp
@@ -7,8 +7,8 @@
 <title>Success</title>
 </head>
 <body>
-	<h1>Welcome , ${name }</h1>
-	<h1>Your email is ${email }</h1>
-	<h2>Your password is ${password } ,Please try to secure it</h2>
+	<h1>Welcome , ${user.name }</h1>
+	<h1>Your email is ${user.email }</h1>
+	<h2>Your password is ${user.password } ,Please try to secure it</h2>
 </body>
 </html>
